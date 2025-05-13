540f71f5a3ea7ffc92b2ee5079433e3c5f48e9b0
Cut Dahliana <cutdahli@mhs.usk.ac.id>
2025-05-13 17:03:14 +0700

    add create and show page, set routes

--- song-view-adonis/start/routes.ts
+++ song-view-adonis/start/routes.ts
@@ -8,5 +8,13 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import LagusController from '#controllers/lagus_controller'
 
-router.on('/').render('pages/home')
+router.get('/', async ({ response }) => {
+  return response.redirect('/lagu')
+})
+
+router.get('lagu', [LagusController, 'index'])
+router.get('lagu/create', [LagusController, 'create'])
+router.post('lagu', [LagusController, 'store'])
+router.get('lagu/:id', [LagusController, 'show'])
